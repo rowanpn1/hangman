fa420bd376be7b264d8397cdc062df9f2b580e04
caswell79 <131504581+caswell79@users.noreply.github.com>
2023-04-21 22:42:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,23 @@ const server = http.createServer((req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify({ message: 'Playing the game...' }));
       break;
+    case '/instructions':
+      // send a plain text response with instructions on how to play the game
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Welcome to the Hangman game! \nTo play the game, simply guess letters one at a time until you can guess the entire word. \nYou can only guess a letter once, so choose carefully! \nYou can play as many times as you like to try and improve your score.');
+      break;
+    case '/highscores':
+      // send a JSON response with the high scores
+      const scores = [
+        { username: 'Alice', score: 10 },
+        { username: 'Bob', score: 8 },
+        { username: 'Charlie', score: 6 },
+        { username: 'Dave', score: 4 },
+        { username: 'Eve', score: 2 }
+      ];
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(scores));
+      break;
     default:
       // send a 404 response if the path is not recognized
       res.setHeader('Content-Type', 'text/plain');
@@ -34,5 +51,5 @@ const server = http.createServer((req, res) => {
 // run command node app.js in terminal to start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("Server listening on port ${PORT}");
+  console.log(`Server listening on port ${PORT}`);
 });
