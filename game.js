d570189f57f4551caa57f7723c6e6d642b240775
caswell79 <131504581+caswell79@users.noreply.github.com>
2023-04-21 23:59:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,78 +1,79 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'hangman';
+// import the words collection from MongoDB
+const db = require("./mongodb").words;
 
-function getRandomWord(callback) {
-  MongoClient.connect(url, (err, client) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const db = client.db(dbName);
-    const words = db.collection('words');
-    words.countDocuments((err, count) => {
-      if (err) {
-        console.error(err);
-        client.close();
-        return;
-      }
-      const index = Math.floor(Math.random() * count);
-      words.findOne({}, { skip: index }, (err, result) => {
-        if (err) {
-          console.error(err);
-          client.close();
-          return;
-        }
-        callback(result.word);
-        client.close();
-      });
-    });
-  });
+// function to generate a random word from the words collection
+async function generateRandomWord() {
+  const count = await db.countDocuments(); // get the total count of documents in the collection
+  const randomIndex = Math.floor(Math.random() * count); // generate a random index based on the count
+  const word = await db.findOne().skip(randomIndex); // get the word at that index
+  return word.word.toLowerCase(); // return the word in lowercase
 }
 
-module.exports = function Game(word) {
-  this.word = word.toUpperCase();
-  this.guesses = new Set();
-  this.guessesLeft = 6;
-  this.gameOver = false;
+// function to create the game object
+function createGame(word) {
+  const game = {
+    word: word,
+    guesses: [],
+    remainingGuesses: 6,
+    status: "playing",
+  };
 
-  this.maskedWord = this.word.split('').map((char) => {
-    if (char.match(/[A-Z]/)) {
-      return '_';
-    }
-    return char;
-  }).join('');
+  // function to get the puzzle display
+  game.getPuzzle = function () {
+    let puzzle = "";
 
-  this.guessLetter = function(letter) {
-    if (this.gameOver) {
-      return;
-    }
-
-    letter = letter.toUpperCase();
-    if (!letter.match(/[A-Z]/)) {
-      return;
-    }
-
-    if (this.guesses.has(letter)) {
-      return;
-    }
-
-    this.guesses.add(letter);
-
-    let found = false;
-    for (let i = 0; i < this.word.length; i++) {
-      if (this.word[i] === letter) {
-        this.maskedWord = this.maskedWord.substr(0, i) + letter + this.maskedWord.substr(i + 1);
-        found = true;
+    // loop through each letter in the word
+    this.word.split("").forEach((letter) => {
+      // if the letter has been guessed, add it to the puzzle display
+      // otherwise, add a "*" to represent the letter
+      if (this.guesses.includes(letter)) {
+        puzzle += letter;
+      } else {
+        puzzle += "*";
       }
+    });
+
+    return puzzle;
+  };
+
+  // function to make a guess
+  game.makeGuess = function (guess) {
+    guess = guess.toLowerCase(); // convert guess to lowercase
+
+    // check if the guess has already been made
+    if (this.guesses.includes(guess)) {
+      console.log("You already guessed that letter.");
+      return;
     }
 
-    if (!found) {
-      this.guessesLeft--;
+    // add the guess to the guesses array
+    this.guesses.push(guess);
+
+    // check if the guess is incorrect
+    if (!this.word.includes(guess)) {
+      console.log("Incorrect guess.");
+      this.remainingGuesses--;
     }
 
-    if (this.guessesLeft === 0 || this.maskedWord === this.word) {
-      this.gameOver = true;
+    // check if the game is over
+    if (this.remainingGuesses === 0) {
+      this.status = "failed";
+      console.log(`Game over. The word was ${this.word}.`);
+    } else if (this.getPuzzle() === this.word) {
+      this.status = "succeeded";
+      console.log(`Congratulations! You guessed the word: ${this.word}.`);
+    } else {
+      console.log(`Guesses: ${this.guesses.join(", ")}`);
+      console.log(`Remaining guesses: ${this.remainingGuesses}`);
+      console.log(`Puzzle: ${this.getPuzzle()}`);
     }
   };
+
+  return game;
+}
+
+// export the generateRandomWord and createGame functions
+module.exports = {
+  generateRandomWord,
+  createGame,
 };
